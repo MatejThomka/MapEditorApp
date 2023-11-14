f18bdb546669c02c68007b7450c817c26ad15ca5
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-14 21:32:46 +0100

    Create TilesList class for storing and showing all tiles in 10 cards.

---
+++ src/main/java/components/panels/TilesList.java
@@ -0,0 +1,29 @@
+package components.panels;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import javax.swing.JPanel;
+
+public class TilesList extends JPanel {
+  private final Tiles[] list = new Tiles[10];
+  private int currentPanel = 0;
+  CardLayout cardLayout = new CardLayout();
+
+  public TilesList() {
+    setLayout(cardLayout);
+    setBackground(Color.BLACK);
+    this.cardLayout.show(this, String.valueOf(currentPanel));
+  }
+
+  public GridBagConstraints positionOnMainFrame() {
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 7;
+    gbc.gridwidth = 2;
+    gbc.gridheight = 2;
+    gbc.anchor = GridBagConstraints.NORTHWEST;
+    gbc.insets.set(0,5,5,5);
+    return gbc;
+  }
+}
