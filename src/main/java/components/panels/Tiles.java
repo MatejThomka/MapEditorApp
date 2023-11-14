a820ba10451ce161797b1d9a5928030c1aced6ab
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-14 21:32:17 +0100

    Create class with Tiles constructor for showing Tiles inside.

---
+++ src/main/java/components/panels/Tiles.java
@@ -0,0 +1,19 @@
+package components.panels;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+public class Tiles extends JPanel {
+
+  public Tiles(){
+    setPreferredSize(new Dimension(1325,212));
+    setLayout(new GridLayout(4, 25));
+    setAlignmentX(LEFT_ALIGNMENT);
+    setAlignmentY(TOP_ALIGNMENT);
+    setBackground(Color.BLACK);
+    setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+  }
+}
