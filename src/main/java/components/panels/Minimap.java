b7427d96521c851084618b4418cd562a6b884fa7
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-14 19:56:48 +0100

    Add constructor and GridBagConstraints for Minimap to be placed in correct position.

--- src/main/java/components/panels/Minimap.java
+++ src/main/java/components/panels/Minimap.java
@@ -1,4 +1,25 @@
 package components.panels;
 
-public class Minimap {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import javax.swing.JPanel;
+
+public class Minimap extends JPanel {
+
+  public Minimap() {
+    setPreferredSize(new Dimension(200, 200));
+    setBackground(Color.BLACK);
+  }
+
+  public GridBagConstraints positionOnMainFrame() {
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.gridx = 2;
+    gbc.gridy = 4;
+    gbc.gridwidth = 4;
+    gbc.gridheight = 2;
+    gbc.anchor = GridBagConstraints.NORTHWEST;
+    gbc.insets.set(0,5,0,5);
+    return gbc;
+  }
 }
