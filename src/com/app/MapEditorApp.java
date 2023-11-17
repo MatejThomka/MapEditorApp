b3eaa52d0f6d83bb125fa5ae82f4c0ce49c1ccd5
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-17 10:33:25 +0100

    Delete init of all Objects for JFrame and move it into Service.

--- src/com/app/MapEditorApp.java
+++ src/com/app/MapEditorApp.java
@@ -1,12 +1,6 @@
 package com.app;
 
-import com.app.components.panels.Editor;
-import com.app.components.panels.Minimap;
-import com.app.components.panels.TilesList;
-import com.app.components.panels.buttons.Fill;
-import com.app.components.panels.buttons.Next;
-import com.app.components.panels.buttons.Previous;
-import com.app.graphic.ImageManager;
+
 import com.app.services.MapEditorService;
 import com.app.services.MapEditorServiceImpl;
 import java.awt.Color;
@@ -16,15 +10,7 @@ import javax.swing.JFrame;
 
 public class MapEditorApp extends JFrame {
 
-  private final ImageManager imageManager = new ImageManager();
-  private final Editor editor = new Editor();
-  private final Minimap minimap = new Minimap();
-  private final TilesList tilesList = new TilesList();
-  private final Next next = new Next(imageManager);
-  private final Previous previous = new Previous(imageManager);
-  private final Fill fill = new Fill(imageManager);
-
-  private final MapEditorService service = new MapEditorServiceImpl(editor, minimap, tilesList, next, previous, fill);
+  private final MapEditorService service = new MapEditorServiceImpl();
 
   public MapEditorApp() {
     setTitle("Map Editor v0.0.2");
