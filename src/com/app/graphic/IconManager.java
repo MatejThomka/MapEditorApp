abd5358ca34e0e57fa98d5499fa27fa96e30f380
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-11-17 10:32:14 +0100

    Create class for managing Icons from buttons.

---
+++ src/com/app/graphic/IconManager.java
@@ -0,0 +1,15 @@
+package com.app.graphic;
+
+import javax.swing.ImageIcon;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class IconManager {
+
+  private ImageIcon icon;
+  private String name;
+}
